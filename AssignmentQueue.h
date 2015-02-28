456d9aa4b4fa854c159b4effbe4647540fc04a90
bradvanwick <cbvzq3@mail.umkc.edu>
2015-02-27 20:36:13 -0600

    what?

---
+++ AssignmentQueue.h
@@ -0,0 +1,42 @@
+#include "Assignment.h"
+#include <queue>
+
+// Assignment Queue
+// Small class that encapsulates a queue of Assignment objects
+
+class AssignmentQueue
+{
+public:
+	AssignmentQueue();
+	Assignment Pop();
+	const bool IsEmpty() const;
+	const int Size() const;
+	void Push(Assignment assignment);
+private:
+	queue<Assignment> _assignments;
+};
+
+AssignmentQueue::AssignmentQueue()
+{ ; }
+
+Assignment AssignmentQueue::Pop()
+{
+	Assignment nextAssignment = _assignments.front();
+	_assignments.pop();
+	return nextAssignment;
+}
+
+const bool AssignmentQueue::IsEmpty() const
+{
+	return _assignments.empty();
+}
+
+const int AssignmentQueue::Size() const
+{
+	return _assignments.size();
+}
+
+void AssignmentQueue::Push(Assignment assignment)
+{
+	_assignments.push(assignment);
+}
